7437badf0f4a94e09cba905a826802369ac14e65
galih-bagus <galihbagus555@gmail.com>
2025-03-05 21:48:53 +0700

    add : login scenario

--- tests/pages/login.page.ts
+++ tests/pages/login.page.ts
@@ -1,5 +1,8 @@
 import {Locator, Page} from '@playwright/test';
 
 export const usernameField = (page: Page): Locator => page.locator(`//input[@data-test='username']`);
-
 export const passwordField = (page: Page): Locator => page.locator(`//input[@data-test='password']`);
+export const buttonSubmit = (page: Page): Locator => page.locator(`//input[@data-test='login-button']`);
+export const errorMessage = (page: Page): Locator => page.locator(`//h3[@data-test='error']`);
+export const home = (page: Page): Locator => page.locator(`//span[@data-test='title']`);
+export const homeFooter = (page: Page): Locator => page.locator(`//div[@data-test='footer-copy']`);
