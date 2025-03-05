21cf2066e921f85f555908acb79e3a1d37cfd804
galih-bagus <galihbagus555@gmail.com>
2025-03-05 21:48:32 +0700

    add data

---
+++ tests/datas/login.data.ts
@@ -0,0 +1,9 @@
+export const VALID_USER = {
+    'username': 'standard_user',
+    'password': 'secret_sauce'
+}
+
+export const INVALID_USER = {
+    'username': 'standarda_user',
+    'password': 'secreta_sauce'
+}
